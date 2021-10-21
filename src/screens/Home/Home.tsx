f5a229364c83649ad426f4da47b92577bce2c283
hazharaziz <hazharaziz5555@gmail.com>
2021-10-21 23:54:07 +0330

    test commit

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,7 @@
-import './Home.css';
-import { motion } from 'framer-motion';
-import { crossfadeAnimation } from '../../animations/motionVariants';
-import Strings from 'global/constants/strings';
+import "./Home.css";
+import { motion } from "framer-motion";
+import { crossfadeAnimation } from "../../animations/motionVariants";
+import Strings from "global/constants/strings";
 
 function HomeScreen() {
   return (
@@ -12,7 +12,7 @@ function HomeScreen() {
         animate="second"
         className="content"
       >
-        <h1 className="fw-light">{Strings.sajaSystem}</h1>
+        <h1 className="fw-light">{Strings.sajaSystemCompleteName}</h1>
         <p className="app-description py-3">
           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
           استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
