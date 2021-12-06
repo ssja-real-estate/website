247e894fc9323e55a02051cbe9337fca7aa5bd15
hazharaziz <hazharaziz5555@gmail.com>
2021-12-07 01:02:58 +0330

    remove edit name from profile

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,15 +1,14 @@
-import Strings from 'global/constants/strings';
-import { defaultGlobalState } from 'global/states/GlobalState';
-import { globalState } from 'global/states/globalStates';
-import { profileModalState } from './ProfileState';
-import User, { defaultUser } from 'global/types/User';
-import { useRef, useState, useEffect } from 'react';
-import { Button, Spinner } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import UserService from 'services/api/UserService/UserService';
-import EditProfileModal from './EditProfileModal';
-import ChangePasswordModal from './ChangePasswordModal';
+import Strings from "global/constants/strings";
+import { defaultGlobalState } from "global/states/GlobalState";
+import { globalState } from "global/states/globalStates";
+import { profileModalState } from "./ProfileState";
+import User, { defaultUser } from "global/types/User";
+import { useRef, useState, useEffect } from "react";
+import { Button, Spinner } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import UserService from "services/api/UserService/UserService";
+import ChangePasswordModal from "./ChangePasswordModal";
 
 const Profile = () => {
   const [modalState, setModalState] = useRecoilState(profileModalState);
@@ -33,20 +32,12 @@ const Profile = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.token]);
 
-  const toggleModalDisplay = (flag: boolean, isEditProfile: boolean = true) => {
-    if (isEditProfile) {
-      setModalState({
-        ...modalState,
-        showEditProfile: flag,
-        showChangePassword: false,
-      });
-    } else {
-      setModalState({
-        ...modalState,
-        showEditProfile: false,
-        showChangePassword: flag,
-      });
-    }
+  const toggleModalDisplay = (flag: boolean) => {
+    setModalState({
+      ...modalState,
+      showEditProfile: false,
+      showChangePassword: flag,
+    });
   };
 
   const loadData = async () => {
@@ -60,7 +51,7 @@ const Profile = () => {
 
   const logout = () => {
     setGlobalState(defaultGlobalState);
-    history.push('/');
+    history.push("/");
   };
 
   return (
@@ -69,32 +60,12 @@ const Profile = () => {
         <Spinner animation="border" variant="primary" className="my-5" />
       ) : (
         <div className="profile-section">
-          {user.name ? (
-            <h1 className="user-name pb-4 fw-light">{user.name}</h1>
-          ) : null}
           <h4 className="user-phone">
             {user.mobile}
             <i className="bi-telephone-fill me-3"></i>
           </h4>
           <div className="d-flex flex-column justify-content-center align-items-center">
-            <EditProfileModal
-              userId={user.id}
-              name={user.name}
-              reloadScreen={loadData}
-            />
             <ChangePasswordModal userId={user.id} reloadScreen={loadData} />
-            <Button
-              variant="outline-secondary"
-              className="rounded-3 px-4 mt-4 w-75"
-              id="edit"
-              name="edit"
-              type="button"
-              onClick={() => {
-                toggleModalDisplay(true);
-              }}
-            >
-              {Strings.editProfile}
-            </Button>
             <Button
               variant="outline-secondary"
               className="rounded-3 px-4 my-4 w-75"
@@ -102,7 +73,7 @@ const Profile = () => {
               name="passwordInput"
               type="button"
               onClick={() => {
-                toggleModalDisplay(true, false);
+                toggleModalDisplay(true);
               }}
             >
               {Strings.changePassword}
