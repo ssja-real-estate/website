b2b87602936cd1bf912d42043cd6e457bd1f4290
hazharaziz <hazharaziz5555@gmail.com>
2022-03-01 11:15:45 +0330

    implement estate api services

--- src/services/api/EstateService/EstateService.ts
+++ src/services/api/EstateService/EstateService.ts
@@ -1,14 +1,78 @@
-import { Estate } from "global/types/Estate";
+import { defaultEstate, Estate } from "global/types/Estate";
 import BaseService from "../BaseService";
 
 class EstateService extends BaseService {
   estateUrl = "/estate";
+
+  async getVerifiedEstates() {
+    let verifiedEstates: Estate[] = [];
+    try {
+      let response = await this.Api.get(
+        `${this.estateUrl}/show/all`,
+        this.config
+      );
+      if (response.data) {
+        verifiedEstates = response.data as Estate[];
+      }
+    } catch (error) {
+      this.handleError(error);
+    }
+
+    return verifiedEstates;
+  }
+
+  async getUnverifiedEstates() {
+    let unVerifiedEstates: Estate[] = [];
+    try {
+      let response = await this.Api.get(
+        `${this.estateUrl}/show/unverified`,
+        this.config
+      );
+      if (response.data) {
+        unVerifiedEstates = response.data as Estate[];
+      }
+    } catch (error) {
+      this.handleError(error);
+    }
+
+    return unVerifiedEstates;
+  }
+
+  async getEstateById(estateId: string) {
+    let estate: Estate = defaultEstate;
+    try {
+      let response = await this.Api.get(
+        `${this.estateUrl}/${estateId}`,
+        this.config
+      );
+      if (response.data) {
+        estate = response.data as Estate;
+      }
+    } catch (error) {
+      this.handleError(error);
+    }
+    return estate;
+  }
+
+  async getUserEstates(userId: string) {
+    let estates: Estate[] = [];
+    try {
+      let response = await this.Api.get(
+        `${this.estateUrl}/estate/user/${userId}`,
+        this.config
+      );
+      if (response.data) {
+        estates = response.data as Estate[];
+      }
+    } catch (error) {
+      this.handleError(error);
+    }
+    return estates;
+  }
+
   async requestAddEtate(estate: Estate, formData: FormData) {
     formData.append("estate", JSON.stringify(estate));
     let newEstate = undefined;
-    console.log("request add");
-    console.log(estate);
-
     try {
       let response = await this.Api.post(this.estateUrl, formData, this.config);
       if (response.data) {
@@ -18,6 +82,19 @@ class EstateService extends BaseService {
       this.handleError(error);
     }
     return newEstate;
+  }
+
+  async verifyEstate(estateId: string) {
+    try {
+      let response = await this.Api.put(
+        `${this.estateUrl}/verify/${estateId}`,
+        this.config
+      );
+      if (response.data) {
+      }
+    } catch (error) {
+      this.handleError(error);
+    }
   }
 }
 
