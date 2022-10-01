3741984d700ccd7fd903d571870489d72a0888ea
soran <soran.taak@gmail.com>
2022-10-01 14:03:54 +0330

    finish signup page

--- utilities/RegexValidator.ts
+++ utilities/RegexValidator.ts
@@ -9,6 +9,26 @@ class RegexValidator {
       error: Pattern.phoneRegex.test(phone) ? "" : "شماره موبایل صحیح نیست!!",
     };
   };
+
+  static validatePassword(password: string) {
+    if (password.length === 0) return false;
+    return password.length >= 8 && password.length <= 16;
+  }
+
+  static validateVerificationCode(code: string) {
+    if (code.length === 0) return false;
+    return code.length === 6;
+  }
+
+  static validateName(name: string) {
+    if (name.length === 0) return false;
+    return name.length >= 3;
+  }
+
+  static validateCode(code: string) {
+    if (code.length === 0) return false;
+    return code.length === 5;
+  }
 }
 
 class Pattern {
