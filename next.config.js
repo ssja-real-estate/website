f956f1ac535c424e91de45cfc5c2e7cb30fb02c8
amanc1361 <rahymrm@gmail.com>
2025-04-28 22:06:39 +0330

    change next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
- 
+   
     output: 'export', // ✅ اضافه شده برای static export
   images: {
-    // loader: "akamai",
-    //path: "",
+    loader: "akamai",
+    path: "",
     domains: ["ssja.ir","www.ssja.ir", "trustseal.enamad.ir"],
     
   },
